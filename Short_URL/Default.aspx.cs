9952dbd670c0e9d160adf13386b1ad91dc78d9de
Petro Ivasenko <petroivan818@gmail.com>
2022-01-20 15:09:09 +0200

    Добавьте файлы проекта.

---
+++ Short_URL/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using Short_URL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Short_URL
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Functions.isValidUrl(url.Value))
+            {
+                string ShortLink = Functions.AddNewLinkToDB(url.Value);
+                //get current domain name
+                string HostName = Request.Url.Host;
+                string domainName = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority);
+                ShortLink = domainName + "/" + ShortLink;
+                data_place.InnerHtml = @"<div class='alert alert-success'><a href ='" + ShortLink + "' target='blanc'>" + ShortLink + "</div>";
+            }
+            else
+            {
+                data_place.InnerHtml = @"<div class='alert alert-danger'>try whith a valid url</div>";
+            }
+        }
+    }
+}
---
+++ Short_URL/Redirect.aspx.cs
@@ -0,0 +1,40 @@
+﻿using Short_URL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Short_URL
+{
+    public partial class Redirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    String ShortLink = "";
+                    ShortLink = Page.RouteData.Values["ShortLink"] as String;
+                    String originalUrl = Functions.GetOriginUrl(ShortLink);
+                    if (originalUrl != null)
+                    {
+                        result_area.InnerHtml = @"<h4>Your link is ready</h4>
+                                        <a class='btn btn-primary' href='" + originalUrl + "'>Get Link</a>";
+                    }
+                    else
+                    {
+                        result_area.InnerHtml = @"<h4>Page Not Found</h4>";
+                    }
+                }
+                catch (Exception)
+                {
+
+                    Response.Redirect("~/");
+                }
+            }
+        }
+    }
+}
